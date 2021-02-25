ae8277ab4bf25ef849ee554ef23eccd774a6f6f0
Tswalano <moganegb@gmail.com>
2021-02-25 20:39:51 +0200

    link module

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,6 @@ export default function Layout({ title,children }) {
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
             <main className="container mx-auto max-w-xl pt-8 min-h-screen">
                 {children}
             </main>
